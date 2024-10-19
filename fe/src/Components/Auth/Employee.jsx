c05fe40a81106e7c683951f686d1457643ac48d1
Ben2811 <luanchuong51@gmail.com>
2024-10-19 15:04:18 +0700

    Add_Signin

--- fe/src/Components/Auth/Employee.jsx
+++ fe/src/Components/Auth/Employee.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Employee = () => {
   return (
-    <div className='employee-container'>
+    <div className="employee-container">
         <div>Hello, Employee</div>
         <div>Let's get to work!!!</div>
     </div>
